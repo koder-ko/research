ffeaa365f86fc7a6e245bafe76972be4efd8b250
koder-ko <bwook0721@gmail.com>
2022-05-26 23:45:21 +0900

    dd

--- light.py
+++ light.py
@@ -1,6 +1,7 @@
 # light는 True => on  False => off
 
 import random
+import copy
 
 class person:
     def __init__(self, typeof: str):
@@ -24,22 +25,36 @@ class person:
 
 
 people_list = [person("B")]
+
 lightfirst = bool(random.randint(0, 1))
 light = lightfirst
-hour = 0
 minit = 0
+average = 0
+time = 0
+a = True
+inptime = int(input("몇번 시행할지 입력:"))
 
 for i in range(1, 100):
     people_list.append(person("A"))
-while True:
-    if people_list[0].people_count == 197:
-        break
-    cur = random.randint(0, 99)
-    target = people_list[cur]
-    light = target.light_control(light)
-    minit += 1
-    if minit == 60:
-        hour += 1
-        minit = 0
 
-print(f"초기 전등: {lightfirst} 걸린 시간) {hour}시간 {minit}분 => {hour/24}일")
+cur_list = copy.deepcopy(people_list)
+
+while True:
+    while a:
+        cur = random.randint(0, 99)
+        target = cur_list[cur]
+        light = target.light_control(light)
+        minit += 1
+        if cur_list[0].people_count == 197:
+            a = False
+    print(minit)
+    average += minit
+    time += 1
+    minit = 0
+    a = True
+    cur_list = copy.deepcopy(people_list)
+    if time == inptime:
+        break
+
+
+print(f"초기 전등: {lightfirst} 시행 횟수: {time} 평균 시행:{average/time}")
